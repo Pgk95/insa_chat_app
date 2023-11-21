909766420daaa0b0a3afd638fc4fe271675edad2
pgk95 <kalupgk@gmail.com>
2023-11-21 22:26:28 +0300

    added some routes

--- app.py
+++ app.py
@@ -11,7 +11,19 @@ socketio = SocketIO(app)
 def index():
     return render_template("index.html")
 
+@app.route('/signup', methods=['POST', 'GET'], strict_slashes=False)
+def signup():
+    return render_template("signup.html")
+
+@app.route('/login', methods=['POST', 'GET'], strict_slashes=False)
+def login():
+    return render_template("login.html")
+
+@app.route('/chat', methods=['POST', 'GET'], strict_slashes=False)
+def chat():
+    return render_template("chat.html")
+
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", debug=True)
+    socketio.run(app, host="0.0.0.0", port="9000", debug=True)
